0857761e912b7e84040bbf4b8a07eabb3147eb42
yozakura-minato <huynhhuulocht@gmail.com>
2026-01-18 12:08:21 +0700

    p1-s1_fix__ csfr config & global exception handle

--- src/main/java/com/yozakura_minato/kensaku_be/config/SecurityConfig.java
+++ src/main/java/com/yozakura_minato/kensaku_be/config/SecurityConfig.java
@@ -3,31 +3,22 @@ package com.yozakura_minato.kensaku_be.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        // @formatter:off
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/auth/**", "/search").permitAll()
                         .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
-        // @formatter:on
-
+                );
         return http.build();
     }
 
---
+++ src/main/java/com/yozakura_minato/kensaku_be/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.yozakura_minato.kensaku_be.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<String> handlingRuntimeException(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> errors = exception.getFieldErrors();
+        if(errors.isEmpty()) {
+            return ResponseEntity.badRequest().body("unknown.valid.exception");
+        }
+        return ResponseEntity.badRequest().body(errors.getFirst().getDefaultMessage());
+    }
+}
