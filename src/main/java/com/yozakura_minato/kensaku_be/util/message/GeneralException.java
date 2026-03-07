213df00eeb2d59bd3629ed8c6f4aafb669923610
yozakura-minato <huynhhuulocht@gmail.com>
2026-03-07 17:18:05 +0700

    p1-s1/feature/sign-in: refactor(global) - move exception messages to util/message

---
+++ src/main/java/com/yozakura_minato/kensaku_be/util/message/GeneralException.java
@@ -0,0 +1,10 @@
+package com.yozakura_minato.kensaku_be.util.message;
+
+/**
+ * Exception message for general
+ */
+public class GeneralException {
+
+    public static final String unknown = "UNKNOWN.GENERAL.EXCEPTION";
+
+}
---
+++ src/main/java/com/yozakura_minato/kensaku_be/util/message/SignUpException.java
@@ -0,0 +1,32 @@
+package com.yozakura_minato.kensaku_be.util.message;
+
+/**
+ * Exception messages for sign up
+ */
+public class SignUpException {
+
+    public static class Email {
+
+        public static final String nulls = "NULL_EMAIL.SIGN_UP.EXCEPTION";
+        public static final String exits = "EMAIL_EXISTS.SIGN_UP.EXCEPTION";
+        public static final String format = "EMAIL_FORMAT.SIGN_UP.EXCEPTION";
+
+    }
+
+    public static class DisplayName {
+
+        public static final String nulls = "NULL_DISPLAY_NAME.SIGN_UP.EXCEPTION";
+        public static final String format = "DISPLAY_NAME_FORMAT.SIGN_UP.EXCEPTION";
+        public static final String length = "DISPLAY_NAME_LENGTH.SIGN_UP.EXCEPTION";
+
+    }
+
+    public static class Password {
+
+        public static final String nulls = "NULL_PASSWORD.SIGN_UP.EXCEPTION";
+        public static final String format = "PASSWORD_FORMAT.SIGN_UP.EXCEPTION";
+        public static final String length = "PASSWORD_LENGTH.SIGN_UP.EXCEPTION";
+
+    }
+
+}
