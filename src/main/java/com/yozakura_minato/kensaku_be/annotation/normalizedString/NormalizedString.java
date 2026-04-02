88568a356246fb0ad2dc5ed39a3a32b4ad977f2a
yozakura-minato <huynhhuulocht@gmail.com>
2026-04-02 18:48:55 +0700

    p1-s1/feature/sign-in: customize annotation to normalize string fields

---
+++ src/main/java/com/yozakura_minato/kensaku_be/annotation/normalizedString/NormalizedString.java
@@ -0,0 +1,17 @@
+package com.yozakura_minato.kensaku_be.annotation.normalizedString;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import tools.jackson.databind.annotation.JsonDeserialize;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>Normalization annotation for String fields of class</p>
+ * Destination value: {@code null} if the plain string is null or contains only spaces, else returns {@code stripped value}.
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonDeserialize(using = StringNormalizingDeserializer.class)
+public @interface NormalizedString {}
