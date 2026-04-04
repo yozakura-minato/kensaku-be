cf765d2cf650b9c033a09b3784baf75e4da59add
yozakura-minato <huynhhuulocht@gmail.com>
2026-04-04 12:45:03 +0700

    p1-s1/feature/sign-in: customize @IsStrongPassword & apply to SignUpRequestDto

--- src/main/java/com/yozakura_minato/kensaku_be/dto/request/SignUpRequestDto.java
+++ src/main/java/com/yozakura_minato/kensaku_be/dto/request/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.yozakura_minato.kensaku_be.dto.request;
 
+import com.yozakura_minato.kensaku_be.annotation.isPasswordStrong.IsStrongPassword;
 import com.yozakura_minato.kensaku_be.annotation.normalizedEmail.NormalizedEmail;
 import com.yozakura_minato.kensaku_be.annotation.normalizedString.NormalizedString;
 import com.yozakura_minato.kensaku_be.exception.message.SignUpException;
@@ -32,28 +33,7 @@ public class SignUpRequestDto {
     @NormalizedString
     @NotBlank(message = SignUpException.Password.nulls)
     @Size(min = 8, max = 50, message = SignUpException.Password.length)
+    @IsStrongPassword
     private String password;
-
-    /**
-     * <p>Validate plain password strength</p>
-     * <p><b>Criteria</b>: The trimmed password must contains at least 2 of below types:</p>
-     * <ul>
-     *     <li>Upper letter</li>
-     *     <li>Lower letter</li>
-     *     <li>Number</li>
-     *     <li>Special character</li>
-     * </ul>
-     * @return Check result
-     */
-    @AssertTrue(message = SignUpException.Password.format)
-    public boolean isPasswordStrength() {
-        String normalizedPassword = password.trim();
-        int typeNumber = 0;
-        typeNumber += normalizedPassword.matches(".*[A-Z].*") ? 1 : 0;
-        typeNumber += normalizedPassword.matches(".*[a-z].*") ? 1 : 0;
-        typeNumber += normalizedPassword.matches(".*[0-9].*") ? 1 : 0;
-        typeNumber += normalizedPassword.matches(".*[^A-Za-z0-9].*") ? 1 : 0;
-        return typeNumber >= 2;
-    }
 
 }
